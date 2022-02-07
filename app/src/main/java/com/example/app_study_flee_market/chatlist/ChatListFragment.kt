faed029ba5de54b1d77d6bdbb532887880a470ed
HyunJin Ko <iamkokoko123@gmail.com>
2022-02-07 17:42:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/app_study_flee_market/chatlist/ChatListFragment.kt
@@ -0,0 +1,14 @@
+package com.example.app_study_flee_market.chatlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.app_study_flee_market.R
+
+class ChatListFragment : Fragment(R.layout.fragment_chat_list) {
+
+
+
+}
